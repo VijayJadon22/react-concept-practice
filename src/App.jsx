43a4629be6f8807995b824ffac18bbcb117c17d0
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-07-26 15:34:09 +0530

    Implemnted debounce functionality on search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import React from "react";
 
+function debounced(func, delay) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => func(...args), delay * 1000);
+  };
+}
+
 export default function App() {
-  return <div>App</div>;
+  const searchFunction = (e) => {
+    const query = e.target.value;
+    console.log(query);
+
+    fetch(`https://example.com/query=${query}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+  return (
+    <div>
+      <h1>This is HomePage</h1>
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={debounced(searchFunction, 2)}
+        // onChange={searchFunction}
+      />
+    </div>
+  );
 }
